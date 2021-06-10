55e96bd66f9a7a2e4af6094622c614fdb9a18eb2
medhanshgit <76427043+medhanshgit@users.noreply.github.com>
2021-06-10 23:06:41 +0530

    Add files via upload

---
+++ amalthea2/2.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+/* Open Menu*/
+function openNav() {
+    document.getElementById("Nav").style.width = "14%";
+}
+
+/* Close Menu*/
+function closeNav() {
+    document.getElementById("Nav").style.width = "0%";
+}
